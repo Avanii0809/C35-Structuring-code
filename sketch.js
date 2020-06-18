09948dce41c76a4fa6fc649a58e54ed0ec5cef3d
Avanii0809 <as080906@gmail.com>
2020-06-18 18:19:03 +0530

    we created the entry form for the game.

--- sketch.js
+++ sketch.js
@@ -1,42 +1,16 @@
-var ball, database, position, pos;
+var database;
+var gameState = 0;
+var playerCount = 0;
+var form, player, game;
 
 function setup(){
     createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
     database = firebase.database();
-    pos = database.ref("ball/position");
-    pos.on("value",readPosition)
+    game = new Game();
+    game.getState();
+    game.start();
 }
 
-function draw(){
-    background("white");
-    if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
-    }
-    else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
-    }
-    drawSprites();
-}
+function draw() {
 
-function changePosition(x,y){
-    //ball.x = ball.x + x;
-    //ball.y = ball.y + y;
-    database.ref("ball/position").set({
-        x: position.x+x,
-        y: position.y+y
-    })
-}
-
-function readPosition(data) {
-    position = data.val();
-    ball.x = position.x;
-    ball.y = position.y;
 }
